6df1eabf505fc3d37fd23873333e4d7fc5176904
Muhammad Pawit <muhamamd.pawit93@gmail.com>
2021-11-10 14:28:42 +0700

    modul po yuna

---
+++ application/controllers/Poyuna.php
@@ -0,0 +1,71 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Poyuna extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		sessionLogin(URLPATH."\\".$this->uri->segment(1));
+		session(dirname(__FILE__)."\\".$this->uri->segment(1).'.php');
+		$this->layout='newtheme/page/main';
+		$this->page='newtheme/page/poyuna/';
+		$this->url=BASEURL.'Poyuna/';
+	}
+
+	public function potongansetoran(){
+		$data=[];
+		$data['title']='Laporan Potongan PO dan Setor Pak Yuna';
+		$get=$this->input->get();
+		if(isset($get['tanggal1'])){
+			$tanggal1=$get['tanggal1'];
+		}else{
+			$tanggal1=null;
+		}
+		if(isset($get['tanggal2'])){
+			$tanggal2=$get['tanggal2'];
+		}else{
+			$tanggal2=null;
+		}
+		if(isset($get['kode_po'])){
+			$kode_po=$get['kode_po'];
+		}else{
+			$kode_po=null;
+		}
+		if(isset($get['refpo'])){
+			$refpo=$get['refpo'];
+		}else{
+			$refpo=null;
+		}
+		$data['tanggal1']=$tanggal1;
+		$data['tanggal2']=$tanggal2;
+		$j=1;
+		$sql="SELECT kbp.*,kbp.kode_po as nama_po,kbp.created_date as tanggalProd, kbp.tim_potong_potongan FROM konveksi_buku_potongan kbp JOIN produksi_po p ON(p.kode_po=kbp.kode_po) WHERE id_potongan > 0 AND p.nama_po LIKE 'PFK%' ";
+		if(empty($kode_po)){
+			if(!empty($tanggal1)){
+				$sql.=" AND date(created_date) BETWEEN '".$tanggal1."' AND '".$tanggal2."' ";
+			}
+		}else{
+			$sql.=" AND kbp.kode_po='".$kode_po."' ";
+		}
+		if(!empty($refpo)){
+			$sql.=" AND refpo='".$refpo."' ";
+		}
+		$sql.=" ORDER BY kbp.created_date DESC ";
+		$results	= $this->GlobalModel->queryManual($sql);
+		$cp=null;
+		$data['prods']=[];
+		foreach($results as $result){
+			$data['prods'][]=array(
+				'no'=>$j,
+				'kode_po'=>$result['kode_po'],
+				'action'=>$action,
+			);
+
+			$j++;
+		}
+
+		$data['page']=$this->page.'potongansetoran';
+
+		$this->load->view($this->layout,$data);
+	}
+}
---
+++ application/views/newtheme/page/poyuna/potongansetoran.php
@@ -0,0 +1,23 @@
+<div class="col-md-12">
+	<div class="form-group">
+		<table class="table table-bordered yessearch">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama PO</th>
+					<th>Jumlah Potongan (Pcs)</th>
+					<th>Jumlah Potongan (Dz)</th>
+					<th>Ongkos Jahit/Lusin</th>
+					<th>Jumlah</th>
+					<th>Jumlah Setor</th>
+					<th>Jumlah Setoran/Lusin</th>
+					<th>Kekurangan Setoran Po Rusak</th>
+					<th>Status</th>
+					<th>Potongan Kekurangan (Rp)</th>
+					<th>Jumlah Bersih (Rp)</th>
+					<th>Ket</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+</div>
