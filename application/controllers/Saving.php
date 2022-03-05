f20728daa95e63c529ba04d9fa7e5c618d463e96
Muhammad Pawit <muhammad.pawit93@gmail.com>
2022-03-05 11:40:28 +0700

    saving

---
+++ application/controllers/Saving.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Saving extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		sessionLogin(URLPATH."\\".$this->uri->segment(1));
+		session(dirname(__FILE__)."\\".$this->uri->segment(1).'.php');
+		$this->layout='newtheme/page/main';
+		$this->page='newtheme/page/saving/';
+		$this->url=BASEURL.'Saving/';
+	}
+
+	public function index(){
+		$data['title']='Saving Pembayaran Tim Potong';
+		$data['products']=[];
+		$no=1;
+		$get=$this->input->get();
+		if(isset($get['tanggal1'])){
+			$tanggal1=$get['tanggal1'];
+		}else{
+			$tanggal1=date('Y-m-d',strtotime("first day of this month"));
+		}
+		if(isset($get['tanggal2'])){
+			$tanggal2=$get['tanggal2'];
+		}else{
+			$tanggal2=date('Y-m-d');;
+		}
+
+		if(isset($get['cmt'])){
+			$tim=$get['cmt'];
+		}else{
+			$tim=null;
+		}
+
+		$data['tanggal1']=$tanggal1;
+		$data['tanggal2']=$tanggal2;
+		$data['tim']=$tim;
+		$sql="SELECT * FROM gaji_timpotong WHERE hapus=0 ";
+		$sql.=" AND DATE(tanggal) BETWEEN '".$tanggal1."' AND '".$tanggal2."' ";
+		if(!empty($tim)){
+			$sql.=" AND timpotong='".$tim."' ";
+		}
+		$data['products']=[];
+		$results=$this->GlobalModel->QueryManual($sql);
+		$no=1;
+		$total=0;
+		$nama=null;
+		foreach($results as $result){
+			$nama=$this->GlobalModel->getdataRow('timpotong',array('id'=>$result['timpotong']));
+			$total+=($result['saving']);
+			$data['products'][]=array(
+				'no'=>$no++,
+				'nama'=>strtolower($nama['nama']),
+				'periode'=>$result['periode'],
+				'jumlah'=>$result['saving'],
+			);
+		}
+		$data['total']=$total;
+		$data['timpotong']=$this->GlobalModel->getData('timpotong',array('hapus'=>0));
+		$data['page']=$this->page.'list';
+		$this->load->view($this->layout,$data);
+	}
+
+}
---
+++ application/views/newtheme/page/saving/list.php
@@ -0,0 +1,109 @@
+<div class="row">
+              <div class="col-sm-2">
+                <div class="form-group">
+                  <label>Tanggal Awal</label>
+                  <input type="text" name="tanggal1" value="<?php echo $tanggal1?>" id="tanggal1" class="form-control">
+                </div>
+              </div>
+              <div class="col-sm-2">
+                <div class="form-group">
+                  <label>Tanggal Akhir</label>
+                  <input type="text" name="tanggal2" value="<?php echo $tanggal2?>" id="tanggal2" class="form-control">
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <label>Tim Potong</label>
+                <select name="tim" id="tim" class="form-control select2bs4" data-live-search="true">
+                  <option value="*">Pilih</option>
+                  <?php foreach($timpotong as $t){?>
+                    <option value="<?php echo $t['id']?>" <?php echo $tim==$t['id']?'selected':'';?>><?php echo $t['nama']?></option>
+                  <?php } ?>
+                </select>
+              </div>
+              <div class="col-sm-2">
+                <div class="form-group">
+                  <label>Action</label><br>
+                  <button onclick="filter()" class="btn btn-info btn-sm">Filter</button>
+                </div>
+              </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <table class="table table-bordered table-hover">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Nama</th>
+          <th>Periode</th>
+          <th>Jumlah (Rp)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($products as $p){?>
+          <tr>
+            <td><?php echo $p['no']?></td>
+            <td><?php echo $p['nama']?></td>
+            <td><?php echo $p['periode']?></td>
+            <td><?php echo number_format($p['jumlah'])?></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="3"><b>Total</b></td>
+          <td><?php echo number_format($total)?></td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+</div>
+<script type="text/javascript">
+  function filter(){
+    var url='?';
+    var tanggal1=$("#tanggal1").val();
+    var tanggal2=$("#tanggal2").val();
+    var tim=$("#tim").val();
+    
+    
+    if(tanggal1){
+      url+='&tanggal1='+tanggal1;
+    }
+
+    if(tanggal2){
+      url+='&tanggal2='+tanggal2;
+    }
+
+    if(tim!='*'){
+      url+='&cmt='+tim;
+    }
+
+
+    location = url;
+  }
+
+  function cetak(){
+     var url='?cetak=1';
+    var tanggal1=$("#tanggal1").val();
+    var tanggal2=$("#tanggal2").val();
+    var tim=$("#tim").val();
+    var jenis=$("#jenis").val();
+    
+    if(tanggal1){
+      url+='&tanggal1='+tanggal1;
+    }
+
+    if(tanggal2){
+      url+='&tanggal2='+tanggal2;
+    }
+
+    if(tim!='*'){
+      url+='&tim='+tim;
+    }
+
+    if(jenis!='*'){
+      url+='&jenis='+jenis;
+    }
+
+    location = url;
+  }
+</script>
