ed79eb9c4e455f1af17810aac57aa8c6fed4098a
Muhammad Pawit <muhammad.pawit93@gmail.com>
2022-03-29 08:16:16 +0700

    updated setorsablon

--- application/controllers/Setoransablon.php
+++ application/controllers/Setoransablon.php
@@ -226,7 +226,7 @@ class Setoransablon extends CI_Controller {
 		}
 		$idcmt=explode("-",$data['namacmt']);
 		//$sql="SELECT k.nosj,kd.* FROM kirimcmtsablon k JOIN kirimcmtsablon_detail kd ON(kd.idkirim=k.id) WHERE idcmt='".$idcmt[0]."' AND k.hapus=0 and kd.hapus=0 AND kd.jumlah_pcs<>kd.totalsetor ";
-		$sql="SELECT k.idcmt,k.nosj,kd.* FROM kirimcmtsablon k JOIN kirimcmtsablon_detail kd ON(kd.idkirim=k.id) WHERE idcmt='".$idcmt[0]."' AND k.hapus=0 and kd.hapus=0 AND kd.kode_po NOT IN (SELECT kode_po FROM setorcmt_sablon_detail) ";
+		$sql="SELECT k.idcmt,k.nosj,kd.* FROM kirimcmtsablon k JOIN kirimcmtsablon_detail kd ON(kd.idkirim=k.id) WHERE idcmt='".$idcmt[0]."' AND k.hapus=0 and kd.hapus=0 AND kd.kode_po NOT IN (SELECT kode_po FROM setorcmt_sablon_detail WHERE hapus=0 ) ";
 		$sj=$this->GlobalModel->queryManual($sql);
 		$i=0;
 		if(!empty($sj)){
