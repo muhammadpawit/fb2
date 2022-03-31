a8c4bcf54078a31faada0a88fbc44f2998f6645f
Muhammad Pawit <muhammad.pawit93@gmail.com>
2022-03-31 09:27:37 +0700

    alokasi mingguan

---
+++ application/controllers/Alokasiperminggu.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Alokasiperminggu extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		sessionLogin(URLPATH."\\".$this->uri->segment(1));
+		session(dirname(__FILE__)."\\".$this->uri->segment(1).'.php');
+		$this->layout='newtheme/page/main';
+		$this->page='newtheme/page/alokasiperminggu/';
+		$this->url=BASEURL.'Alokasiperminggu/';
+		$this->load->model('AdjustModel');
+	}
+
+	public function index(){
+		$data['title']='Alokasi Mingguan ';
+		$data['page']=$this->page.'list';
+		$this->load->view($this->layout,$data);
+	}
+}
---
+++ application/views/newtheme/page/alokasiperminggu/list.php
@@ -0,0 +1,7 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="form-group">
+      <iframe src="https://docs.google.com/spreadsheets/d/1Il5bKewIi7W6KW0DfBB248Jf3E2H8dCNrHdVD0Kw2g0/edit?usp=sharing" class="full" style="height:800px"></iframe>
+    </div>
+  </div>
+</div>
