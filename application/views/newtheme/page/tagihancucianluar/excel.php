5d9859b1f58ceee9a408d7e0cbe955248afa6cfa
m-pawit <mochpawit28@hotmail.com>
2022-11-02 16:57:22 +0700

    tambah ttd dan registered

--- application/views/newtheme/page/tagihancucianluar/excel.php
+++ application/views/newtheme/page/tagihancucianluar/excel.php
@@ -3,6 +3,14 @@ $namafile=$title.'_'.time();
 header("Content-type: application/vnd-ms-excel");
 header("Content-Disposition: attachment; filename=".$namafile.".xls");
 ?>
+<style type="text/css">
+  @import url('https://fonts.googleapis.com/css2?family=Baskervville:ital@1&display=swap');
+  .registered {
+    font-family: 'Baskervville', serif;
+  }
+
+  .besar {font-size: 18px;}
+</style>
 <h2><?php echo $title?> Forboys  Production</h2>					
 					<table border="1" style="width: 100%;border-collapse: collapse;">
 						<thead>
@@ -41,3 +49,78 @@ header("Content-Disposition: attachment; filename=".$namafile.".xls");
 							</tr>
 						</tfoot>
 					</table>
+
+					<table>
+
+                                        <tr>
+                                            <th colspan="5"></th>
+                                            <th>Disetujui</th>
+                                            <th>Di Periksa oleh:</th>
+                                            <th>Di Buat oleh:</th>
+
+                                        </tr>
+
+                                        <tr align="center">
+                                            <td colspan="5"></td>
+                                            <td><b>SPV</b></td>
+                                            <td><b>ADM Keuangan</b></td>
+                                            <td><b>ADM Gudang</b></td>
+
+                                        </tr>
+
+                                        <tr>
+                                            <td colspan="5"></td>
+                                            <td height="100" align="center">
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                ( Muchlas )
+
+                                            </td>
+                                             <td height="100" align="center">
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                ( Dinda )
+
+                                            </td>
+                                            <td height="100" align="center">
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                <br>
+
+                                                ( Dewi )
+
+                                            </td>
+
+                                        </tr>
+
+                                        <tr>
+                                            <td colspan="5"></td>
+                                        </tr>
+                                        <tr>
+                                          <td colspan="8" align="right"><i class="registered">Registered by Forboys Production System <?php echo date('d-m-Y H:i:s'); ?></i></td>
+                                        </tr>
+                                    </table>
