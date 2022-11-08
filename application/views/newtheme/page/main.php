9ad1842a8b4549143cf725db09714571ba4f8165
m-pawit <mochpawit28@hotmail.com>
2022-11-08 15:30:56 +0700

    updated

--- application/views/newtheme/page/main.php
+++ application/views/newtheme/page/main.php
@@ -260,7 +260,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.18
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+    <strong>Copyright &copy; 2020-<?php echo date('Y') ?> <a href="<?php echo BASEURL ?>">Forboys Production System</a>.</strong> All rights
     reserved.
   </footer>
 
