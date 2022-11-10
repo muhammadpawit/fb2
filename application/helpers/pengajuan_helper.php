ce88df14b349dcfa7165afc2b12590331a56924f
m-pawit <mochpawit28@hotmail.com>
2022-11-10 10:04:21 +0700

    updated

--- application/helpers/pengajuan_helper.php
+++ application/helpers/pengajuan_helper.php
@@ -328,7 +328,7 @@
 
 	function MenuSub2($parent_id){
 		$CI =& get_instance();
-		$menu=$CI->GlobalModel->queryManual('SELECT * FROM menu WHERE parent_id='.$parent_id.' AND hapus=0 AND id IN(SELECT menuid FROM usermenu WHERE userid='.callSessUser('id_user').') ORDER BY id ASC');
+		$menu=$CI->GlobalModel->queryManual('SELECT * FROM menu WHERE parent_id='.$parent_id.' AND hapus=0 AND id IN(SELECT menuid FROM usermenu WHERE userid='.callSessUser('id_user').') ORDER BY nama ASC');
 		return $menu;
 	}
 
