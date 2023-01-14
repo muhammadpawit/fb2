aa7d8f396ade8c8afe46d5fcf540a4536314153e
m-pawit <mochpawit28@hotmail.com>
2023-01-14 17:10:45 +0700

    fix hpp

--- application/views/finishing/hpp/hpp-detail.php
+++ application/views/finishing/hpp/hpp-detail.php
@@ -87,7 +87,7 @@
 
                                         <?php //if (!empty($produk['gambar_po'])){ ?>
 
-                                            <img src="<?php echo BASEURL.$po['gambar_po'] ?>" style="height: 50vh: 100%;"  >
+                                            <img src="<?php echo BASEURL.$po['gambar_po'] ?>" style="height: 50vh:width:100%;"  >
 
                                         <?php //} else { ?>
 
