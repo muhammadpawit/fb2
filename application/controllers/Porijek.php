cc272cc4d96d5f361921c19f7d56fef5af0d2405
mpawit-mac <muhammad.pawit93@gmail.com>
2023-04-05 09:07:56 +0700

    pi rijek

---
+++ application/controllers/Porijek.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Porijek extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		//sessionLogin(URLPATH."\\".$this->uri->segment(1));
+		//session(dirname(__FILE__)."\\".$this->uri->segment(1).'.php');
+		$this->page='newtheme/page/porijek/';
+		$this->url=BASEURL.'Porijek/';
+		$this->load->model('ReportModel');
+		$this->load->model('M_potonganoperator');
+		$this->layout='newtheme/page/main';
+		$this->login 		= BASEURL.'login';
+		$this->auth 	= $this->session->userdata('id_user');
+		if(empty($this->auth)) {redirect($this->login);}
+	}
+
+	function index(){
+		$data 			=[];
+		$data['title']	='Po Rijek';
+		$data['prods']	=[];
+		$data['no']		=1;
+		$data['prods']	=$this->GlobalModel->getData('rijek',array());
+		$data['action']	=$this->url.'save';
+		$data['page']	=$this->page.'list';
+		$this->load->view($this->layout,$data);
+	}
+
+	function save(){
+		$post = $this->input->post();
+		$po=explode("-", $post['kode_po']);
+		//pre($po);
+		$insert=array(
+			'idpo'	=>$po[0],
+			'kode_po'=>$po[1],
+			'pcs'=>$post['pcs'],
+			'keterangan'=>$post['keterangan']
+		);
+		$this->db->insert('rijek',$insert);
+		$this->session->set_flashdata('msg','Data Berhasil Disimpan');
+		redirect($this->url);
+	}
+}
---
+++ application/views/newtheme/page/porijek/list.php
@@ -0,0 +1,84 @@
+<div class="row">
+  <div class="col-md-6">
+    <div class="form-group">
+        <!-- Trigger the modal with a button -->
+        <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#myModal">Tambah</button>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="form-group">
+        <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Kode PO</th>
+                <th>Jumlah (Pcs)</th>
+                <th>Keterangan</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach($prods as $p){ ?>
+                <tr>
+                  <td><?php echo $no++;?></td>
+                  <td><?php echo $p['kode_po']?></td>
+                  <td><?php echo $p['pcs']?></td>
+                  <td><?php echo $p['keterangan']?></td>
+                </tr>
+              <?php } ?>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Tambah Baru</h4>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="<?php echo $action ?>">
+          <div class="row">
+            <div class="col-md-12">
+              <label>Kode PO</label>
+              <select name="kode_po" id="kode_po" class="form-control select2bs4 autopoid" style="width: 100%">
+                
+              </select>
+            </div>
+            <div class="col-md-12">
+              <label>Jumlah Pcs</label>
+              <input type="text" name="pcs" class="form-control" value="0">
+            </div>
+            <div class="col-md-12">
+              <label>Keterangan</label>
+              <textarea name="keterangan" class="form-control" required></textarea>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <br>
+              <button type="submit" class="btn btn-success full btn-sm">Simpan</button>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group"><br>
+              <button type="button" class="btn btn-default btn-sm full" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        
+      </div>
+    </div>
+
+  </div>
+</div>
