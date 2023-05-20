6171283ebc9ba54784e477d5d3a6599fda46d1e9
m-pawit <mochpawit28@hotmail.com>
2023-05-20 17:05:31 +0700

    fixed

--- application/controllers/Rekapkasbon.php
+++ application/controllers/Rekapkasbon.php
@@ -42,7 +42,7 @@ class Rekapkasbon extends CI_Controller {
 		foreach($results as $k){
 			$divisi=$this->GlobalModel->GetDataRow('divisi',array('hapus'=>0,'id'=>$k['divisi']));
 			$kasbon=$this->KasbonModel->kasbon($bulan,$tahun,$k['id']);
-			$pinjaman = $this->GlobalModel->GetDataRow('pinjaman',array('hapus'=>0,'idkaryawan'=>$k['id']));
+			$pinjaman = $this->GlobalModel->GetDataRow('pinjaman_karyawan',array('hapus'=>0,'idkaryawan'=>$k['id']));
 			$data['kar'][]=array(
 				'no'=>$no,
 				'id'=>$k['id'],
