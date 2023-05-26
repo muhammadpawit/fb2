6f7200e09659bf9fa02015a9c3a6f204472813bc
m-pawit <mochpawit28@hotmail.com>
2023-05-26 13:06:41 +0700

    fix finishing

--- application/controllers/Masterdata.php
+++ application/controllers/Masterdata.php
@@ -169,7 +169,9 @@ class Masterdata extends CI_Controller {
 	public function price_packing_json()
 	{
 		$post = $this->input->get();
-		$jenis=substr($post['kodepo'],0,3);
+		$po = $this->GLobalModel->GetDataRow('produksi_po',array('kode_po'=>$post['kode_po']));
+		//$jenis=substr($post['kodepo'],0,3);
+		$jenis=!empty($po) ? $po['nama_po'] : '';
 		$data = $this->GlobalModel->getDataRow('master_jenis_po',array('nama_jenis_po'=>$jenis));
 		echo json_encode($data);
 	}
