c2df0f529790f73619adf20a99e9a98d33870d9e
m-pawit <mochpawit28@hotmail.com>
2023-05-31 10:20:29 +0700

    perubahan admin

--- application/views/produksi/kirimcmt_pdf.php
+++ application/views/produksi/kirimcmt_pdf.php
@@ -137,7 +137,7 @@
 						<tr>
 						<td align="center" height="100" valign="bottom">(..................)</td>
 						<td align="center" height="100" valign="bottom">(MUCHLAS)</td>
-						<td align="center" height="100" valign="bottom">(DINDA)</td>
+						<td align="center" height="100" valign="bottom">(NISA)</td>
 						</tr>
 					</table>
 				</td>
