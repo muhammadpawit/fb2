6a77cb9bcff754532f1d20440cc344334aae5adf
mpawit-mac <muhammad.pawit93@gmail.com>
2023-07-05 13:15:54 +0700

    updated

--- application/controllers/Pembayaran.php
+++ application/controllers/Pembayaran.php
@@ -752,13 +752,13 @@ class Pembayaran extends CI_Controller {
 		if(isset($get['tanggal1'])){
 			$tanggal1=$get['tanggal1'];
 		}else{
-			//$tanggal1=date('Y-m-d',strtotime("first day of previous month"));
-			$tanggal1=null;
+			$tanggal1=date('Y-m-d',strtotime("first day of this month"));
+			//$tanggal1=null;
 		}
 		if(isset($get['tanggal2'])){
 			$tanggal2=$get['tanggal2'];
 		}else{
-			$tanggal2=null;
+			$tanggal2=date('Y-m-d');
 		}
 
 		if(isset($get['cmt'])){
