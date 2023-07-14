8099b40f4d7a6ab64334db01e4fff37fd555aa6a
mpawit-mac <muhammad.pawit93@gmail.com>
2023-07-14 11:10:10 +0700

    updated ajuan alat-alat

--- application/views/newtheme/page/ajuanalatalat/edit.php
+++ application/views/newtheme/page/ajuanalatalat/edit.php
@@ -1,3 +1,16 @@
 <form method="post" action="<?php echo $action ?>">
 	<input type="hidden" name="id" value="<?php echo $prods['id'] ?>">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="form-group">
+				<label>Nama Barang</label>
+				<select name="id_persediaan" class="form-control select2bs4" disabled readonly>
+					<?php foreach($barang as $b){ ?>
+						<option value="<?php echo $b['id_persediaan']?>"
+							<?php echo $b['id_persediaan']==$prods['id_persediaan']?'selected':'';?>><?php echo $b['nama_item']?></option>
+					<?php } ?>
+				</select>
+			</div>
+		</div>
+	</div>
 </form>
