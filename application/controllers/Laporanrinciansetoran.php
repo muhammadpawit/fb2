3c27944047a0d040acf53b4cd665465e3a58a50e
mpawit-mac <muhammad.pawit93@gmail.com>
2023-08-24 10:14:26 +0700

    updated fixed

--- application/controllers/Laporanrinciansetoran.php
+++ application/controllers/Laporanrinciansetoran.php
@@ -56,8 +56,8 @@ class Laporanrinciansetoran extends CI_Controller {
 			if(!empty($cabang)){
 				$lokasi=$this->GlobalModel->GetDataRow('lokasi_cmt',array('id'=>$cabang['lokasi']));
 			}
-			$potong=$this->GlobalModel->GetDataRow('konveksi_buku_potongan',array('kode_po'=>$r['kode_po']));
-			$pcs_kirim=$this->GlobalModel->QueryManualRow("SELECT SUM(jumlah_pcs) as total FROM kirimcmt_detail WHERE kode_po='".$r['kode_po']."' ");
+			$potong=$this->GlobalModel->GetDataRow('konveksi_buku_potongan',array('hapus'=>0,'kode_po'=>$r['kode_po']));
+			$pcs_kirim=$this->GlobalModel->QueryManualRow("SELECT SUM(jumlah_pcs) as total FROM kirimcmt_detail WHERE kode_po='".$r['kode_po']."' AND hapus=0 ");
 			$data['prods'][]=array(
 				'no'=>$no,
 				'tanggal'=>date('d/m/Y',strtotime($r['created_date'])),
