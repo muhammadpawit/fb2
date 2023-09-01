80d27a4613b01cc5245bd9b7e92dfe9764b605a8
mpawit-mac <muhammad.pawit93@gmail.com>
2023-09-01 09:48:59 +0700

    updated excel po rijek dan bangke

--- application/views/newtheme/page/laporanporijek/rijek_excel.php
+++ application/views/newtheme/page/laporanporijek/rijek_excel.php
@@ -19,27 +19,39 @@
 			<thead>
 				<tr>
 					<th>No</th>
+					<th>Tanggal</th>
+					<th>Nama CMT</th>
 					<th>Kode PO</th>
 					<th>Jumlah Bangke (pcs)</th>
 					<th>Jumlah Rijek (pcs)</th>
+					<th>Keterangan</th>
 				</tr>
 			</thead>
 			<tbody>
-				<?php $total=0;$rijek=0;?>
+			<?php $total=0;$rijek=0;?>
 				<?php foreach($prods as $p){?>
 					<tr>
 						<td><?php echo $p['no']?></td>
+						<td><?php echo $p['tanggal']?></td>
+						<td><?php echo $p['cmt']?></td>
 						<td><?php echo $p['kode_po']?></td>
-						<td><?php echo $p['bangke']?></td>
-						<td><?php echo $p['rijek']?></td>
+						<td align="center"><?php echo $p['bangke']?></td>
+						<td align="center"><?php echo $p['rijek']?></td>
+						<td><?php echo $p['keterangan']?></td>
 					</tr>
 				<?php $total+=($p['bangke']);?>
 				<?php $rijek+=($p['rijek']);?>
 				<?php } ?>
 				<tr>
-					<td colspan="2"><b>Total</b></td>
-					<td><b><?php echo $total ?></b></td>
-					<td><b><?php echo $rijek ?></b></td>
+					<td align="center" colspan="4"><b>Total</b></td>
+					<td align="center"><b><?php echo $total ?></b></td>
+					<td align="center"><b><?php echo $rijek ?></b></td>
+				</tr>
+				<tr>
+					
+					<td align="right" colspan="7">
+					<i class="registered">Registered by Forboys Production System <?php echo date('d-m-Y H:i:s'); ?></i>
+					</td>
 				</tr>
 			</tbody>
 		</table>
