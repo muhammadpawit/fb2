8cd36e7f56836637bcb9adb6008d46b083fa362a
mpawit-mac <muhammad.pawit93@gmail.com>
2023-09-30 09:37:52 +0700

    laporan po rijek celana

--- application/controllers/Laporanporijek.php
+++ application/controllers/Laporanporijek.php
@@ -74,4 +74,64 @@ class Laporanporijek extends CI_Controller {
 			$this->load->view($this->layout,$data);
 		}
 	}
+
+	public function celana(){
+		$data['title']='Laporan PO Rijek';
+		$get = $this->input->get();
+		if(isset($get['jenis'])){
+			$jenis=$get['jenis'];
+		}else{
+			$jenis=null;
+		}
+		$data['jenis']=$jenis;
+		if(!empty($jenis)){
+			
+			$join=' LEFT JOIN master_jenis_po mjp ON mjp.nama_jenis_po=p.nama_po ';
+			$where=' AND mjp.idjenis= '.$jenis;
+		}else{
+			$join='';
+			$where=' ';
+		}
+		$sql ="SELECT p.kode_po,SUM(krs.barang_cacad_qty) as rijek,SUM(krs.bangke_qty) as bangke FROM kelolapo_rincian_setor_cmt_celana krs JOIN produksi_po p ON (p.id_produksi_po=krs.idpo) $join where p.hapus=0 $where ";
+		
+		//$sql.=" AND barang_cacad_qty > 0 OR bangke_qty > 0  ";
+		$sql.=" AND bangke_qty > 0  ";
+		$sql.=" GROUP BY idpo ORDER BY krs.created_date ASC ";
+		$results=$this->GlobalModel->QueryManual($sql);
+		//pre($results);
+		$data['prods']=[];
+		$no=1;
+		$rjk=0;
+		$kembali=0;
+		$bangke=0;
+		foreach($results as $r){
+			$rjk=$this->GlobalModel->QueryManualRow("SELECT COALESCE(SUM(pcs),0) as total FROM rijek where kode_po='".$r['kode_po']."' ");
+			$bangke=$this->GlobalModel->QueryManualRow("SELECT COALESCE(SUM(rincian_bangke),0) as total FROM kelolapo_rincian_setor_cmt_finish_celana where kode_po='".$r['kode_po']."' ");
+			$keterangan_bangke=$this->GlobalModel->QueryManualRow("SELECT created_date,rincian_keterangan as keterangan FROM kelolapo_rincian_setor_cmt_finish_celana where kode_po='".$r['kode_po']."' AND rincian_keterangan IS NOT NULL and rincian_keterangan <>'-' ");
+			$cmt=$this->GlobalModel->QueryManualRow(" 
+				SELECT * FROM kelolapo_rincian_setor_cmt_celana WHERE kode_po='".$r['kode_po']."'
+			");
+			$kembali=$this->GlobalModel->QueryManualRow("SELECT COALESCE(SUM(qty),0) as total FROM pengembalian_bangke where hapus=0 and kode_po='".$r['kode_po']."' ");
+			$sisa = $bangke['total']-$kembali['total'];
+			
+			if($sisa<>0){
+				$data['prods'][]=array(
+					'no'=>$no++,
+					'kode_po'=>$r['kode_po'],
+					'bangke'=>$sisa,
+					'keterangan' => $keterangan_bangke['keterangan'],
+					'tanggal' => date('d/m/Y',strtotime($cmt['created_date'])),
+					'cmt'	=> $cmt['nama_cmt'],
+					'rijek'=>$rjk['total'],
+					'sisa' => $sisa,
+				);
+			}
+		}
+		if(isset($get['excel'])){
+			$this->load->view($this->page.'rijek_excel',$data);
+		}else{
+			$data['page']=$this->page.'rijek';
+			$this->load->view($this->layout,$data);
+		}
+	}
 }
