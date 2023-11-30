1c03edf7a856cd150cab20dfcec116b6b438d4fd
m-pawit <mochpawit28@hotmail.com>
2023-11-30 15:37:22 +0700

    acc ajuan alat kemeja

--- application/controllers/Gudang.php
+++ application/controllers/Gudang.php
@@ -156,7 +156,7 @@ class Gudang extends CI_Controller {
 		if(isset($get['spv'])){
 			$data['page']=$this->page.'gudang/pengajuan/mingguan_list_spv_kemeja';
 		}else{
-			$data['page']=$this->page.'gudang/pengajuan/mingguan_list_kemeja';
+			$data['page']=$this->page.'gudang/pengajuan/mingguan_list';
 		}
 		//pre($data['products']);
 		$data['acc_ajuan_mingguan']=$this->GlobalModel->QueryManualRow("SELECT tanggal FROM acc_ajuan_mingguan WHERE DATE(tanggal)='".$tanggal1."' ORDER BY tanggal DESC LIMIT 1");
