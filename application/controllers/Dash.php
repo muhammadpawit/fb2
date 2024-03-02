03847b0f3fe43d3981d1aa0092f11f1117cd4b11
m-pawit <mochpawit28@hotmail.com>
2024-03-02 15:59:58 +0700

    update total pending po di warning

--- application/controllers/Dash.php
+++ application/controllers/Dash.php
@@ -568,7 +568,7 @@ class Dash extends CI_Controller {
 			);
 		}
 		$data['reqharga']=$this->GlobalModel->getData('request_harga',array('status'=>0));
-		$data['popending'] = ($this->ReportModel->BeredarPo(null,'SABLON')+$this->ReportModel->BeredarPo(null,'BORDIR')+$this->ReportModel->KLOPo('2'));
+		$data['popending'] = ($this->ReportModel->BeredarPo(null,'SABLON')+$this->ReportModel->BeredarPo(null,'BORDIR')+$this->ReportModel->KLOPo('kaos'));
 		$data['page']=$this->page.'/dash/welcome';
 		$this->load->view($this->page.'main',$data);
 	}
