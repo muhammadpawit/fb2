41029da592deb7cda8b1d31dc960f08d7ba7a59f
m-pawit <mochpawit28@hotmail.com>
2024-06-28 15:24:34 +0700

    sj potrait

--- application/controllers/Kelolapo.php
+++ application/controllers/Kelolapo.php
@@ -2329,7 +2329,7 @@ class Kelolapo extends CI_Controller {
 	        //$paper = 'A4';
 	        $paper = array(0,0,800,850);
 	        //orientasi paper potrait / landscape
-	        $orientation = "landscape";
+	        $orientation = "potrait";
 	        
 			$this->load->view('laporan_pdf',$this->data, true);	    
 	        
