ac8e79168dd91bc45a7734c74a3f8abde6872245
m-pawit <mochpawit28@hotmail.com>
2024-08-19 11:31:26 +0700

    vview nota

--- application/views/newtheme/page/gudang/pengajuan/cetak.php
+++ application/views/newtheme/page/gudang/pengajuan/cetak.php
@@ -472,10 +472,14 @@
                             </table>
 
                             <!-- <img src="" style="width:100%"> -->
-                             <iframe src="<?php echo BASEURL?>uploads/nota/<?php echo $parent['dokumenNota']?>" frameborder="0" height="500"></iframe>
+                             
                         </div>
                     </div>
-
+                    <div class="row">
+                        <div class="col-md-6">
+                        <iframe src="<?php echo BASEURL?>uploads/nota/<?php echo $parent['dokumenNota']?>" frameborder="0" height="500" style="width:100%"></iframe>
+                        </div>
+                    </div>
 
 
                     <div class="hidden-print mt-4 mb-4 no-print">
