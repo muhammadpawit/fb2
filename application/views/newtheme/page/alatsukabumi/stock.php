ab32ef5d2412e008472d06c6ceb41f2fd1c92505
m-pawit <mochpawit28@hotmail.com>
2024-08-26 14:14:01 +0700

    update stok skb

--- application/views/newtheme/page/alatsukabumi/stock.php
+++ application/views/newtheme/page/alatsukabumi/stock.php
@@ -1,4 +1,24 @@
 <div class="row">
+	<div class="col-md-4">
+		<div class="form-group">
+			<label>Tanggal Awal</label>
+			<input type="text" name="tanggal1" id="tanggal1" value="<?php echo $tanggal1?>" class="form-control">
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="form-group">
+			<label>Tanggal Akhir</label>
+			<input type="text" name="tanggal2" id="tanggal2" value="<?php echo $tanggal2?>" class="form-control">
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="form-group">
+			<label>Aksi</label><br>
+			<button class="btn btn-info btn-sm" onclick="filtertglonly()">Filter</button>
+		</div>
+	</div>
+</div>
+<div class="row">
 	<div class="col-md-12">
 		<table class="table table-bordered yessearch">
 			<thead>
