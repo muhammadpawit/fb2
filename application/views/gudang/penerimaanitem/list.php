434101e745e3e0a2c5bddce7c8fd6a8e0fe84177
m-pawit <mochpawit28@hotmail.com>
2024-08-28 10:11:35 +0700

    update : validasi penerimaan item

--- application/views/gudang/penerimaanitem/list.php
+++ application/views/gudang/penerimaanitem/list.php
@@ -101,12 +101,13 @@
                         <?php if(akseshapus()==1){?>
                           <a href="<?php echo BASEURL?>Gudang/penerimaanitem_hapus/<?php echo $p['id']?>" class="btn btn-danger btn-xs">Hapus</a>
                         <?php } ?>
-                        
-                        <?php if($p['validasi']==0 && $setujui==1){ ?>
+                        <?php if($setujui==1){ ?>
+                        <?php if($p['validasi']==0){ ?>
                           <a href="<?php echo BASEURL?>Gudang/validasi/<?php echo $p['id']?>" onclick="return confirm('Apakah yakin ?')" class="btn btn-warning btn-xs">Validasi</a>
                           <?php }else{ ?>
                               <span class="btn btn-success btn-xs">Sudah divalidasi</span>
                             <?php } ?>
+                          <?php } ?>
                       </td>
                     </tr>
                   <?php } ?>
