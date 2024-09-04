747b31995d801acf41c0cc5f244556d690614269
m-pawit <mochpawit28@hotmail.com>
2024-09-04 09:32:35 +0700

    update hpp

--- application/controllers/Finishing.php
+++ application/controllers/Finishing.php
@@ -1511,6 +1511,13 @@ class Finishing extends CI_Controller {
 					'keterangan'=>'Packing',
 				),
 			);
+		}else if(strtolower($namapo)=="fshk"){
+			$viewData['packing']=array(
+				array(
+					'harga_dz'=>30000, // perubahan ke 30000 tgl 2 februari 2024
+					'keterangan'=>'Packing',
+				),
+			);
 		}else{
 			$viewData['packing']= $this->GlobalModel->getData('packing',array('nama_po'=>$kodepo,'hapus'=>0));
 		}
