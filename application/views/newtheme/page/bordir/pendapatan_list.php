405a4fd9aee39a25a252ddd50b5d60903db05a2b
M Pawit <muhammad.pawit93@gmail.com>
2024-10-19 14:46:28 +0700

    debugs

--- application/views/newtheme/page/bordir/pendapatan_list.php
+++ application/views/newtheme/page/bordir/pendapatan_list.php
@@ -37,13 +37,13 @@
     <table class="table table-bordered table-striped">
     <thead>
         <tr style="background-color:yellow">
-            <th>No. Mesin</th>
+            <th>No.Mesin</th>
             <th>Shift</th>
             <th>Stich</th>
             <th>0.15</th>
             <th>0.18</th>
             <?php foreach($luar as $l){ ?>
-                <th><?php echo $l['perkalian'] . ' ' . $l['nama'] ?></th>
+                <th><?php echo $l['perkalian'] .' '.$l['nama']?></th>
             <?php } ?>
             <th>Jml Per Mesin (Rp)</th>
             <th>Pendapatan Per Mesin (Rp)</th>
@@ -52,61 +52,62 @@
     </thead>
     <tbody>
         <?php 
-        $total_g02 = 0; 
-        $permesin = [];
-        $grand_total = 0; // Total pendapatan semua mesin
-        ?>
-        <?php foreach($products as $p){ ?>
-            <?php 
-            // Akumulasi pendapatan per mesin berdasarkan nomor mesin
-            if (!isset($permesin[$p['nomesin']])) {
-                $permesin[$p['nomesin']] = 0;
+        $total_per_mesin = [];
+        $grand_total = 0; // Total keseluruhan pendapatan
+        
+        // Step 1: Hitung total per mesin untuk setiap shift pagi dan malam
+        foreach ($products as $p) {
+            if (!isset($total_per_mesin[$p['nomesin']])) {
+                $total_per_mesin[$p['nomesin']] = 0;
             }
-            $permesin[$p['nomesin']] += $p['pendapatan']; // Akumulasi per mesin
-            ?>
-        <?php } ?>
 
-        <?php $j = 0; ?>
-        <?php foreach($products as $p){ ?>
+            // Tambahkan pendapatan shift ke total mesin
+            $total_per_mesin[$p['nomesin']] += $p['pendapatan'];
+        }
+        
+        $j = 0;
+        foreach ($products as $p): 
+            // Step 2: Menampilkan data di tabel
+        ?>
             <tr>
-                <td>Mesin <?php echo $p['nomesin'] ?></td>
-                <td><?php echo $p['shift'] ?></td>
-                <td align="right"><?php echo number_format($p['stich']) ?></td>
+                <td>Mesin <?php echo $p['nomesin']; ?></td>
+                <td><?php echo $p['shift']; ?></td>
+                <td align="right"><?php echo number_format($p['stich'])?></td>
                 <td align="right"><?php echo number_format($p['0.15']); ?></td>
-                <td align="right"><?php echo number_format($p['0.18']) ?></td>
+                <td align="right"><?php echo number_format($p['0.18'])?></td>
+                
+                <?php foreach($luar as $b): ?>
+                    <td align="right">
+                        <?php 
+                            // Dapatkan data pendapatan luar jika ada
+                            $hasil = json_encode($this->ReportModel->total02_array($p['nomesin'],$p['shift'],$p['tanggal1'],$p['tanggal2'],$b['idpemilik']));
+                            $data = json_decode($hasil);
+                            echo isset($data->data) ? number_format($data->data) : 0;
+                        ?>
+                    </td>
+                <?php endforeach; ?>
 
-                <!-- Pendapatan PO luar -->
-                <?php foreach($luar as $b){ ?>
+                <td align="right"><?php echo number_format($p['pendapatan']); ?></td>
+                
                 <td align="right">
                     <?php 
-                    $hasil = json_encode($this->ReportModel->total02_array($p['nomesin'],$p['shift'],$p['tanggal1'],$p['tanggal2'],$b['idpemilik']));
-                    $data = json_decode($hasil);
-                    if (isset($data->data)) {
-                        echo number_format($data->data); 
-                        $total_g02 += $data->data;
+                    // Step 3: Hanya tampilkan total pendapatan per mesin di shift malam
+                    if ($p['shift'] == 'MALAM' && isset($total_per_mesin[$p['nomesin']])) {
+                        echo number_format($total_per_mesin[$p['nomesin']]);
+                        $grand_total += $total_per_mesin[$p['nomesin']]; // Tambahkan ke grand total
+                    } else {
+                        echo 0;
                     }
                     ?>
                 </td>
-                <?php } ?>
-
-                <td align="right"><?php echo number_format($p['pendapatan']) ?></td>
-
-                <!-- Tampilkan jumlah pendapatan per mesin untuk semua shift -->
-                <td align="right">
-                    <?php echo number_format($permesin[$p['nomesin']]); ?>
-                    <?php $grand_total += $permesin[$p['nomesin']]; // Akumulasi grand total ?>
-                </td>
-                
                 <td></td>
             </tr>
-            <?php $j++; ?> 
-        <?php } ?>
-
-        <!-- Baris total pendapatan -->
+        <?php endforeach; ?>
+        
+        <!-- Step 4: Tampilkan total keseluruhan di baris paling bawah -->
         <tr>
-            <td colspan="6"><b>Total</b></td>
-            <td align="right"><?php echo number_format($total_g02) ?></td>
-            <td align="right"><?php echo number_format($grand_total) ?></td>
+            <td colspan="7" align="right"><b>Total</b></td>
+            <td align="right"><?php echo number_format($grand_total); ?></td>
             <td></td>
         </tr>
     </tbody>
