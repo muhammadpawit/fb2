e00c1f0a52f63358bf35f7ea8c479df032b20db3
M Pawit <muhammad.pawit93@gmail.com>
2024-11-09 14:10:39 +0700

    update

--- application/views/newtheme/page/user/menu.php
+++ application/views/newtheme/page/user/menu.php
@@ -8,91 +8,63 @@
 	<input type="hidden" name="user_menu[]" value="1">
 	<input type="hidden" name="user_menu[]" value="2">
 	<input type="hidden" name="user_menu[]" value="113">
-<div class="row">
-			<?php foreach(MenuParentuser() as $mp){?>
-
-			<div class="col-md-4">
-
-			<table class="table table-bordered">
-
-				<thead>
-
-					<tr>
-
-						<th>
-							<?php if(in_array($mp['id'],$inmenu)){?>
-							<input type="checkbox" name="user_menu[]" value="<?php echo $mp['id']?>" checked>&nbsp;<?php echo $mp['nama']?>
-							<?php }else{?>
-								<input type="checkbox" name="user_menu[]" value="<?php echo $mp['id']?>">&nbsp;<?php echo $mp['nama']?>
-							<?php } ?>
-
-						</th>
-
-					</tr>
-
-				</thead>
-
-				<tbody>
-
-					<?php foreach( MenuSub1All($mp['id']) as $sub1 ){?>
-
-					<tr>
-
-						<td>
-							<?php if(in_array($sub1['id'],$inmenu)){?>
-							<input type="checkbox" name="user_menu[]" value="<?php echo $sub1['id']?>" checked> <?php echo $sub1['nama']?>
-							<?php }else{ ?>
-							<input type="checkbox" name="user_menu[]" value="<?php echo $sub1['id']?>"> <?php echo $sub1['nama']?>
-							<?php } ?>
-						</td>
-
-						<?php foreach( MenuSub2All($sub1['id']) as $sub2 ){?>
-
-							<tr>
-
-								<td>
-									<?php if(in_array($sub2['id'],$inmenu)){?>
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="user_menu[]" value="<?php echo $sub2['id']?>" checked> <?php echo $sub2['nama']?>
-									<?php }else{?>
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="user_menu[]" value="<?php echo $sub2['id']?>"> <?php echo $sub2['nama']?>
-									<?php } ?>
-								</td>
-
-								<?php foreach( MenuSub3All($sub2['id']) as $sub3 ){?>
-
-									<tr>
-
-										<td>
-											<?php if(in_array($sub3['id'],$inmenu)){?>
-										&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="user_menu[]" value="<?php echo $sub3['id']?>" checked> <?php echo $sub3['nama']?>
-											<?php }else{?>
-										&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="user_menu[]" value="<?php echo $sub3['id']?>"> <?php echo $sub3['nama']?>
-											<?php } ?>
-										</td>
-
-									</tr>
-
-								<?php } ?>
-
-							</tr>
-
-						<?php } ?>
-
-
-
-					</tr>
-
-					<?php } ?>
-
-				</tbody>
-
-			</table>
-
-			</div>
-
-			<?php } ?>
-
+	<div class="box-group" id="accordion">
+    <?php foreach (MenuParentuser() as $mp) { ?>
+        <div class="panel box box-primary">
+            <div class="box-header with-border">
+                <h4 class="box-title">
+                    <input type="checkbox" name="user_menu[]" value="<?php echo $mp['id']; ?>" <?php echo in_array($mp['id'], $inmenu) ? 'checked' : ''; ?>>
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $mp['id']; ?>" aria-expanded="true" class="collapsed">
+                        &nbsp;<?php echo $mp['nama']; ?>
+                    </a>
+                </h4>
+            </div>
+            <div id="collapse<?php echo $mp['id']; ?>" class="panel-collapse collapse">
+                <div class="box-body">
+                    <?php foreach (MenuSub1All($mp['id']) as $sub1) { ?>
+                        <div class="panel box box-success">
+                            <div class="box-header with-border">
+                                <h4 class="box-title">
+                                    <input type="checkbox" name="user_menu[]" value="<?php echo $sub1['id']; ?>" <?php echo in_array($sub1['id'], $inmenu) ? 'checked' : ''; ?>>
+                                    <a data-toggle="collapse" data-parent="#collapse<?php echo $mp['id']; ?>" href="#collapseSub1<?php echo $sub1['id']; ?>" class="collapsed">
+                                        &nbsp;<?php echo $sub1['nama']; ?>
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseSub1<?php echo $sub1['id']; ?>" class="panel-collapse collapse">
+                                <div class="box-body">
+                                    <?php foreach (MenuSub2All($sub1['id']) as $sub2) { ?>
+                                        <div class="panel box box-warning">
+                                            <div class="box-header with-border">
+                                                <h4 class="box-title">
+                                                    <input type="checkbox" name="user_menu[]" value="<?php echo $sub2['id']; ?>" <?php echo in_array($sub2['id'], $inmenu) ? 'checked' : ''; ?>>
+                                                    <a data-toggle="collapse" data-parent="#collapseSub1<?php echo $sub1['id']; ?>" href="#collapseSub2<?php echo $sub2['id']; ?>" class="collapsed">
+                                                        &nbsp;<?php echo $sub2['nama']; ?>
+                                                    </a>
+                                                </h4>
+                                            </div>
+                                            <div id="collapseSub2<?php echo $sub2['id']; ?>" class="panel-collapse collapse">
+                                                <div class="box-body">
+                                                    <?php foreach (MenuSub3All($sub2['id']) as $sub3) { ?>
+                                                        <div>
+                                                            <input type="checkbox" name="user_menu[]" value="<?php echo $sub3['id']; ?>" <?php echo in_array($sub3['id'], $inmenu) ? 'checked' : ''; ?>>
+                                                            &nbsp;<?php echo $sub3['nama']; ?>
+                                                        </div>
+                                                    <?php } ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
 </div>
+
 <div class="row">
 	<div class="col-md-12">
 		<label><button type="submit" class="btn btn-info btn-sm">Simpan</button></label>
