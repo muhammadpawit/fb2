54392bd2f2a82d94369d776277336b7d3dcac243
M Pawit <muhammad.pawit93@gmail.com>
2024-11-23 14:22:13 +0700

    update

--- application/controllers/Sablon.php
+++ application/controllers/Sablon.php
@@ -290,7 +290,9 @@ class Sablon extends CI_Controller {
 			"SELECT * FROM gaji_sablon_harian WHERE hapus=0 AND LOWER(periode)='".strtolower($post['periode'])."' ";
 			// cek tim berdasarkan cmt
 		if($post['cmt']==87){
-			$query .=" AND id_karyawan_harian IN (193,194,213) ";
+			$query .=" AND id_karyawan_harian IN (193,194) ";
+		}else if($post['cmt']==31){
+			$query .=" AND id_karyawan_harian IN (213) ";
 		}else if($post['cmt']==19){
 			$query .=" AND id_karyawan_harian IN (195,196,205) ";
 		}else{
