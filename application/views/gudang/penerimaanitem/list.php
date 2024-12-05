74549ca2f966fbc09a007898446c33c94df7d8c4
M Pawit <muhammad.pawit93@gmail.com>
2024-12-05 10:29:00 +0700

    update

--- application/views/gudang/penerimaanitem/list.php
+++ application/views/gudang/penerimaanitem/list.php
@@ -96,7 +96,9 @@
                       <td><?php echo strtolower($p['nama']) ?></td>
                       <td align="center"><?php echo $p['jumlah']?></td>
                       <td><?php echo $p['harga']?></td>
-                      <td><?php echo ($p['harga']*$p['jumlah'])?></td>
+                      <?php if($results['jenis']==1){?>
+                      <td><?php echo ($p['harga']*$p['ukuran'])?></td>
+                      <?php } ?>
                       <td><?php echo strtolower(!empty($p['keterangan'])?$p['keterangan']:'') ?></td>
                       <td class="right">
                         <?php foreach ($i['action'] as $action) { ?>
