38001ab690ea000ddbe1e86566aa7850045bcd8c
m-pawit <muhammad.pawit93@gmail.com>
2025-06-04 21:02:32 +0700

    oto

--- application/helpers/pengajuan_helper.php
+++ application/helpers/pengajuan_helper.php
@@ -260,7 +260,8 @@
 		if(!empty($menu)){
 			$satuan= ($menu['waktu'] > 59)?' jam':' menit';
 			$waktu = ($menu['waktu'] > 59)?$menu['waktu']/60:$menu['waktu'];
-			$hasil = 'Anda diberikan otorisasi data selama ' .$waktu. $satuan.' dan berakhir pada pukul '.date('H:i:s',strtotime($menu['batas'])).' WIB.';
+			$hasil = 'Anda diberikan otorisasi data selama ' .$waktu. $satuan;
+			$hasil .= ' dan berakhir pada pukul '.date('H:i:s',strtotime($menu['batas'])).' WIB.';
 		}
 		return $hasil;
 
