c6dd7d6c56e7c6a672d7aa0ecf0fc5c1d90de059
m-pawit <muhammad.pawit93@gmail.com>
2025-06-10 09:41:11 +0700

    add edit bagian in mutasi

--- application/controllers/Keuangan.php
+++ application/controllers/Keuangan.php
@@ -3,6 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Keuangan extends CI_Controller {
 
+	public $layout;
+	public $page;
+	public $url;
+	public $login;
+	public $auth;
+	public $session;
+	public $GlobalModel;
+	public $input;
+	public $db;
+	public $ReportModel;
+	public $upload;
+	public $viewData;
+	public $pdfgenerator;
+	public $pagination;
+	public $uri;
+	public $pdf;
+	public $data;
+	public $bg_warning;
+	public $bg_danger;
+	public $bg_success;
+	public $bg_info;
+
 	function __construct() {
 		parent::__construct();
 		//sessionLogin(URLPATH."\\".$this->uri->segment(1));
@@ -1259,65 +1281,37 @@ class Keuangan extends CI_Controller {
 			echo '<form id="editForm" method="POST" action="'.BASEURL.'Keuangan/mutasieditsave">';
             echo '<input type="hidden" name="id" value="' . $data['id'] . '">';
             echo '<div class="form-group">';
-            // echo '<label for="tanggal">Tanggal:</label>';
-            // echo '<input type="date" name="tanggal" class="form-control" value="' . $data['tanggal'] . '">';
-            // echo '</div>';
-
-            // echo '<div class="form-group">';
-            // echo '<label for="tgltransaksi">Tanggal Transaksi:</label>';
-            // echo '<input type="date" name="tgltransaksi" class="form-control" value="' . $data['tgltransaksi'] . '">';
-            // echo '</div>';
-
-            // echo '<div class="form-group">';
-            // echo '<label for="bank_id">Bank ID:</label>';
             echo '<input type="hidden" name="bank" class="form-control" value="' . $data['bank_id'] . '">';
-            // echo '</div>';
-
             echo '<div class="form-group">';
+			echo '<div class="form-group">';
+			echo '<label>Bagian</label>';
+			echo '<select name="cat" id="cat" class="form-control select2bs4">';
+			echo '<option value="*"' . ($data['bagian'] == '*' ? ' selected' : '') . '>Semua</option>';
+			echo '<option value="0"' . ($data['bagian'] == '0' ? ' selected' : '') . '>Default</option>';
+			echo '<option value="1"' . ($data['bagian'] == '1' ? ' selected' : '') . '>Konveksi</option>';
+			echo '<option value="2"' . ($data['bagian'] == '2' ? ' selected' : '') . '>Bordir</option>';
+			echo '<option value="3"' . ($data['bagian'] == '3' ? ' selected' : '') . '>Sablon</option>';
+			echo '</select>';
+			echo '</div>';
             echo '<label for="saldoawal">Saldo Awal:</label>';
             echo '<input type="number" name="saldoawal" class="form-control" value="' . $data['saldoawal'] . '">';
             echo '</div>';
-
             echo '<div class="form-group">';
             echo '<label for="saldomasuk">Saldo Masuk:</label>';
             echo '<input type="number" name="saldomasuk" class="form-control" value="' . $data['saldomasuk'] . '">';
             echo '</div>';
-
             echo '<div class="form-group">';
             echo '<label for="saldokeluar">Saldo Keluar:</label>';
             echo '<input type="number" name="saldokeluar" class="form-control" value="' . $data['saldokeluar'] . '">';
             echo '</div>';
-
             echo '<div class="form-group">';
             echo '<label for="saldo">Saldo:</label>';
             echo '<input type="number" name="saldo" class="form-control" value="' . $data['saldo'] . '">';
             echo '</div>';
-
             echo '<div class="form-group">';
             echo '<label for="keterangan">Keterangan:</label>';
             echo '<textarea name="keterangan" class="form-control">' . $data['keterangan'] . '</textarea>';
             echo '</div>';
-
-            // echo '<div class="form-group">';
-            // echo '<label for="referensi">Referensi:</label>';
-            // echo '<input type="text" name="referensi" class="form-control" value="' . $data['referensi'] . '">';
-            // echo '</div>';
-
-            // echo '<div class="form-group">';
-            // echo '<label for="bagian">Bagian:</label>';
-            // echo '<input type="text" name="bagian" class="form-control" value="' . $data['bagian'] . '">';
-            // echo '</div>';
-
-            // echo '<div class="form-group">';
-            // echo '<label for="hapus">Hapus:</label>';
-            // echo '<input type="checkbox" name="hapus" ' . ($data['hapus'] ? 'checked' : '') . '>';
-            // echo '</div>';
-
-            // echo '<div class="form-group">';
-            // echo '<label for="pengalokasian">Pengalokasian:</label>';
-            // echo '<input type="text" name="pengalokasian" class="form-control" value="' . $data['pengalokasian'] . '">';
-            // echo '</div>';
-
             echo '<button type="submit" class="btn btn-primary">Simpan</button>';
             echo '</form>';
         } else {
@@ -1336,6 +1330,7 @@ class Keuangan extends CI_Controller {
 				'saldokeluar' => $post['saldokeluar'],
 				'saldo' => $post['saldo'],
 				'keterangan' => $post['keterangan'],
+				'bagian' => $post['bagian'],
 			),
 			array('id'=>$post['id'])
 		);
