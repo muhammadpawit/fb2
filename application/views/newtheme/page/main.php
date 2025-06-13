17cf8d9831bc3cdbbaf664e893e84ee678a61cd1
m-pawit <muhammad.pawit93@gmail.com>
2025-06-13 15:50:51 +0700

    user panel css

--- application/views/newtheme/page/main.php
+++ application/views/newtheme/page/main.php
@@ -219,7 +219,7 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
       <!-- Sidebar user panel -->
-      <div class="user-panel" style="height: 6vw;">
+      <div class="user-panel" style="height: 8vh;">
         <div class="pull-left image">
           <img src="<?php echo foto(callSessUser('id_user')) ?>" class="img-circle" alt="User Image">
         </div>
