c26c3f4f0e85d1cfa77ae13179672f2342793ce7
m-pawit <muhammad.pawit93@gmail.com>
2025-10-17 20:16:55 +0700

    update login alert..

--- application/views/login_view_modern.php
+++ application/views/login_view_modern.php
@@ -374,7 +374,7 @@
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <?php echo $this->session->flashdata('gagal'); ?> 
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+                
             </button>
         </div>
         <?php } ?>
