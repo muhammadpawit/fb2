996b3fdc624eb2787e282d18cff13ed29ce6f2c6
m-pawit <muhammad.pawit93@gmail.com>
2025-12-17 08:39:48 +0700

    update report packing

--- application/models/Report.php
+++ application/models/Report.php
@@ -89,7 +89,7 @@ class Report extends CI_Model {
 
 	function packing($type){
 		$sql ="SELECT * FROM `packing` WHERE id_produksi_po NOT IN (SELECT idpo FROM finishing_kirim_gudang) and hapus=0";
-		$sql.=" AND date(creted_date) BETWEEN '".date('Y-m-d',strtotime("Monday this week"))."' AND '".date('Y-m-d',strtotime("Saturday this week"))."' ";
+		$sql.=" AND date(creted_date) BETWEEN '".date('Y-m-d',strtotime("Monday last week"))."' AND '".date('Y-m-d',strtotime("Saturday this week"))."' ";
 		$sql.=" ORDER BY creted_date DESC ";
 		$data = $this->GlobalModel->QueryManual($sql);
 		if($type=='count'){
