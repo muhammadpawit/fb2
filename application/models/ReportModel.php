a8f790bb9906fa5da3b61eb84da166c450235f42
M Pawit <muhammad.pawit93@gmail.com>
2026-02-14 12:06:44 +0700

    fixe pembayaran

--- application/models/ReportModel.php
+++ application/models/ReportModel.php
@@ -1767,8 +1767,14 @@ class ReportModel extends CI_Model {
 		if(!empty($d['lusin'])){
 			return $d['lusin'];
 		}else{
-			$d=$this->GlobalModel->QueryManualRow("SELECT SUM(banyak_lapis_potongan) as lusin FROM konveksi_buku_potongan_variasi WHERE idpo='".$kode_po."' ");	
-			return !empty($d)?$d['lusin']:0;
+			$d=$this->GlobalModel->QueryManualRow("SELECT SUM(dz_variasi) as lusin FROM konveksi_buku_potongan WHERE hapus=0 AND idpo='".$kode_po."' ");
+			if(!empty($d['lusin'])){
+				return $d['lusin'];
+			}else{
+				$d=$this->GlobalModel->QueryManualRow("SELECT SUM(banyak_lapis_potongan) as lusin FROM konveksi_buku_potongan_variasi WHERE idpo='".$kode_po."' ");	
+				return !empty($d)?$d['lusin']:0;
+			}
+			
 		}
 	}
 
