5199d5b6b8775c7625d7574cd84c391b0adbbee4
M Pawit <muhammad.pawit93@gmail.com>
2026-04-16 10:28:18 +0700

    perbaikan query

--- application/controllers/Keuangan.php
+++ application/controllers/Keuangan.php
@@ -620,12 +620,12 @@ class Keuangan extends CI_Controller {
 		if(!empty($karyawan)){
 			// $products=$this->GlobalModel->getData('pinjaman_karyawan',array('hapus'=>0,'idkaryawan'=>$karyawan));
 			$sql ="SELECT * FROM pinjaman_karyawan WHERE hapus=0 AND idkaryawan='".$karyawan."'";
-			$sql.=" ORDER BY status ASC";
+			$sql.=" ORDER BY status ASC, tanggal DESC";
 			$products=$this->GlobalModel->QueryManual($sql);
 		}else{
 			// $products=$this->GlobalModel->getData('pinjaman_karyawan',array('hapus'=>0));
 			$sql ="SELECT * FROM pinjaman_karyawan WHERE hapus=0";
-			$sql.=" ORDER BY status ASC ";
+			$sql.=" ORDER BY status ASC, tanggal DESC ";
 			$products=$this->GlobalModel->QueryManual($sql);
 		}
 		
