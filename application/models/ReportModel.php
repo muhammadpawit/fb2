298a2762e7ab6a6c47c12c3a4e143ad81faa0d74
M Pawit <muhammad.pawit93@gmail.com>
2026-06-06 16:44:28 +0700

    update

--- application/models/ReportModel.php
+++ application/models/ReportModel.php
@@ -1833,6 +1833,7 @@ class ReportModel extends CI_Model {
                 JOIN gaji_operator_detail_new godn ON godn.idgaji = gon.id
                 WHERE gon.hapus=0 AND godn.hapus=0 
                 AND DATE(gon.tanggal2) BETWEEN '$tanggal1' AND '$tanggal2'
+                AND gon.idkaryawan NOT IN (115,144,147)
                 AND gon.idgajiopt = (
                     SELECT MAX(idgajiopt) 
                     FROM gaji_operator_new gon2 
